46fd0143aef2d516be2ab2079201e1f856d6966c
leoke599 <leoke073@gmail.com>
2025-01-15 10:51:57 -0700

    Added all script functionality Reminder: add documentation and error handling

--- note.sh
+++ note.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+notes=notes.txt
+case $1 in
+    list) 
+        # list notes
+        cat $notes;;
+
+    add)
+        # add notes
+        message=$2
+        if [ "message" != "" ]; then
+            timestamp=$(date +"%Y-%m-%d %H:%M:%S -")
+            note="$timestamp $message"
+            echo $note >> $notes
+            echo "Note added successfully"
+        fi;;
+
+    search)
+        # search option
+        search=$2
+        if [ "search" != "" ]; then
+            grep $search $notes
+        fi;;
+
+    *)
+        # invalid option
+        echo "Error: Invalid option"
+        exit;;
+esac
